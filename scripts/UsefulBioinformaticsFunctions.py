175f853f0d347316d29b01a8d39ecedef94cd964
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-03-03 11:45:21 -0800

    Update Reverse Compliment
    Function is now robust to capital and lowercase options and 'N' values.  The reverser map is included in the main body of the script to prevent regeneration every time the function is called.

--- scripts/UsefulBioinformaticsFunctions.py
+++ scripts/UsefulBioinformaticsFunctions.py
@@ -1,8 +1,9 @@
 #Create a dictionary that converts from one base to its reverse compliment
-reverser = {'A':'T','T':'A','G':'C','C':'G'}
+reverser = {'A':'T','T':'A','G':'C','C':'G','N':'N',
+            'a':'t','t':'a','g':'c','c':'g','n':'n'}
 
 def reverseCompliment(DNA):
-    
+
     #Reverse the string using a nifty map!
     reverseComplimentMap = map(lambda base: reverser[base], DNA[::-1])
     reverseComplimentList = list(reverseComplimentMap)
