9e1f0bf64bf6606d4ecf82a002580a89bae04480
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-03-03 11:47:46 -0800

    Created bedToFasta Function
    Added this function to this script as it was being used in multiple other scripts with almost identical implementation.

--- scripts/UsefulBioinformaticsFunctions.py
+++ scripts/UsefulBioinformaticsFunctions.py
@@ -1,3 +1,5 @@
+import subprocess
+
 #Create a dictionary that converts from one base to its reverse compliment
 reverser = {'A':'T','T':'A','G':'C','C':'G','N':'N',
             'a':'t','t':'a','g':'c','c':'g','n':'n'}
@@ -13,3 +15,10 @@ def reverseCompliment(DNA):
 #Determines whether or not a given base is a purine.
 def isPurine(nucleotide: str) -> bool:
     return (nucleotide.upper() == "G" or nucleotide.upper() == "A")
+
+def bedToFasta(bedFilePath, genomeFilePath, fastaOutputFilePath):
+    "Uses bedtools to convert a bed file to fasta format."
+
+    print("Calling shell subprocess to use bedtools to generate a fasta file from the given bed file...")
+    subprocess.run(" ".join(["bedtools","getfasta","-s","-name","-fi",genomeFilePath,
+        "-bed",bedFilePath,">",fastaOutputFilePath]), shell = True, check = True)
