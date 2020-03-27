764013454a34be22cc7f2e92c0aa52a5487a3836
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-03-26 18:48:42 -0700

    Added option to stratify samples by microsatellite stability
    - Updated sort function to output to the same filepath that was inputted, negating the need for multiple filepaths.
    - Created a class to handle the file system for this scripts many output forms, which were quickly getting out of hand.
    - Added the option to stratify samples by microsatellite stability
    - Added the option to output results in individual donor files in addition to one anonymous file.

--- python_scripts/ParseICGC.py
+++ python_scripts/ParseICGC.py
@@ -3,7 +3,7 @@
 import os, gzip, subprocess
 from TkinterDialog import TkinterDialog, Selections
 from UsefulBioinformaticsFunctions import reverseCompliment, isPurine
-from typing import IO
+from typing import IO, List
 
 
 # This class represents the mutation data obtained from ICGC in a more precise form.
@@ -80,7 +80,8 @@ class ICGCIterator:
         self.ICGCFile = ICGCFile # The file containing the ICGC data
         self.finishedDonors = list() # A list of donors to make sure we don't encounter one more than once.
         self.currentDonor = '' # The donorID currently being read for mutation data.
-        self.mutations = dict() # A list of mutations unique to the current donor, to avoid writing duplicate mutations.
+        self.currentDonorMutations = dict() # A dictionary of mutations unique to the current donor, to avoid writing duplicate mutations.
+        self.previousDonorMutations: List[ICGCMutation] = list() # A list that keeps the last donor's mutations in order to write data on individual donors all at once.
 
     # Make the class iteratable, returning unique data lines one at a time.
     def __iter__(self):
@@ -115,24 +116,104 @@ class ICGCIterator:
                 if choppedUpLine[1] != self.currentDonor:
                     self.finishedDonors.append(self.currentDonor)
                     self.currentDonor = choppedUpLine[1]
-                    self.mutations.clear()
+                    self.previousDonorMutations = list(self.currentDonorMutations.values())
+                    self.currentDonorMutations.clear()
                     if self.currentDonor in self.finishedDonors:
                         raise ValueError("Error:  Donor " + self.currentDonor + " is present in more than one block of data!")
                     print("Reading and writing from donor",self.currentDonor)
 
                 # Have we seen this mutation in this donor?
-                if choppedUpLine[0] not in self.mutations:
-                    self.mutations[choppedUpLine[0]] = None
-                    return ICGCMutation(choppedUpLine)
+                if choppedUpLine[0] not in self.currentDonorMutations:
+                    newMutation = ICGCMutation(choppedUpLine)
+                    self.currentDonorMutations[choppedUpLine[0]] = newMutation
+                    return newMutation
+
+
+# This class handles the (somewhat complex) file systems for the parsing process and makes output file paths easily acessible.
+# It takes the directory of the ICGC file as input as well as boolean values to determine how the data is being parsed
+# which determines what directories and file paths need to be generated.
+class ICGCParserFileManager:
+
+    def __init__(self, ICGCFilePath, convertToBed: bool, convertToMSIseq: bool, separatebyMSI: bool, createIndividualDonorFiles: bool):
+
+        self.localRootDirectory = os.path.dirname(ICGCFilePath) # The root directory for this localized file system
+        self.mutationGroupName = os.path.split(ICGCFilePath)[1].rsplit('.',3)[-3] # The name of the mutation group the directory pertains to
+
+        if convertToBed: 
+            self.prepForBedFileOutput(createIndividualDonorFiles)
+            if separatebyMSI: self.prepForMSISeparation(createIndividualDonorFiles)
+
+        if convertToMSIseq: self.prepForMSIseqFileOutput()
+
+
+    # Prepare the file system for bed output by generating the paths to the bed ouput files.
+    # (Optionally, also prepare the system to output individual donor files)
+    def prepForBedFileOutput(self, createIndividualDonorFiles):
+
+        self.bedFilePath = os.path.join(self.localRootDirectory,self.mutationGroupName+"_singlenuc_context.bed")
+
+        if createIndividualDonorFiles:
+
+            self.individualDonorsDirectory = os.path.join(self.localRootDirectory,"individual_donors")
+            if not os.path.exists(self.individualDonorsDirectory): os.mkdir(self.individualDonorsDirectory)
+
+
+    # Prepare the file system to separate bed files based on microsatellite stability and generate the paths to output files.
+    # (Optionally, also prepare the system to output individual donor files)
+    def prepForMSISeparation(self, createIndividualDonorFiles):
+        
+        microsatteliteDirectory = os.path.join(self.localRootDirectory,"microsatellite_analysis")
+        if not os.path.exists(microsatteliteDirectory): os.mkdir(microsatteliteDirectory)
+
+        self.MSIMutationGroupName = "MSI_" + self.mutationGroupName
+        self.MSSMutationGroupName = "MSS_" + self.mutationGroupName
+
+        self.MSIBedFilePath = os.path.join(microsatteliteDirectory,self.MSIMutationGroupName+"_singlenuc_context.bed")
+        self.MSSBedFilePath = os.path.join(microsatteliteDirectory,self.MSSMutationGroupName+"_singlenuc_context.bed")
+
+        if createIndividualDonorFiles:
+
+            self.individualMSIDonorsDirectory = os.path.join(microsatteliteDirectory,"individual_donors","MSI")
+            self.individualMSSDonorsDirectory = os.path.join(microsatteliteDirectory,"individual_donors","MSS")
+
+            if not os.path.exists(self.individualMSIDonorsDirectory): os.makedirs(self.individualMSIDonorsDirectory)
+            if not os.path.exists(self.individualMSSDonorsDirectory): os.makedirs(self.individualMSSDonorsDirectory)
+
+
+    # Prepare the file system for MSIseq output by generating the path to the MSIseq data file.
+    def prepForMSIseqFileOutput(self):
+        self.MSIseqDataFilePath = os.path.join(self.localRootDirectory,self.mutationGroupName+"_MSIseq_data.tsv")
+
+
+    # Generates a file path for the given donor and returns it.
+    # If MSI is NoneType, the file is made without specification of microsatellite stability.
+    # Otherwise, the MSI argument dictates whether the donr is classified as microsatellite instable (true) or stable (false).
+    def getIndividualDonorFilePath(self, donorID: str, MSI = None):
+
+        if MSI is None: 
+            donorDirectory = os.path.join(self.individualDonorsDirectory,donorID)
+            donorFile = donorID + "_" + self.mutationGroupName + "_singlenuc_context.bed"
+        elif MSI: 
+            donorDirectory = os.path.join(self.individualMSIDonorsDirectory,donorID)
+            donorFile = donorID + "_" + self.MSIMutationGroupName + "_singlenuc_context.bed"
+        else: 
+            donorDirectory = os.path.join(self.individualMSSDonorsDirectory,donorID)
+            donorFile = donorID + "_" + self.MSSMutationGroupName + "_singlenuc_context.bed"
+
+        if not os.path.exists(donorDirectory): os.mkdir(donorDirectory)
+        return os.path.join(donorDirectory,donorFile)
 
 
 #Create the Tkinter UI
 dialog = TkinterDialog(workingDirectory=os.path.join(os.path.dirname(__file__),"..","data"))
 dialog.createMultipleFileSelector("ICGC Mutation Files:",0,("gzip files",".gz"))
-dialog.createCheckbox("Convert to Bed SNPs", 1, 0, 2)
-dialog.createCheckbox("Convert to MSIseq format", 1, 2, 2)
-dialog.createReturnButton(2,0,2)
-dialog.createQuitButton(2,2,2)
+dialog.createCheckbox("Convert to Bed SNPs", 1, 0)
+dialog.createCheckbox("Convert to MSIseq format", 1, 1)
+dialog.createCheckbox("Also Create individual bed files for each donor.",2,0)
+dialog.createMultipleFileSelector("MSI Donor Lists (Optional)",3,("text files",".txt"))
+dialog.createCheckbox("Also Separate Bed Mutations by Microsatellite Stability",4,0)
+dialog.createReturnButton(5,0)
+dialog.createQuitButton(5,2)
 
 # Run the UI
 dialog.mainloop()
@@ -143,9 +224,23 @@ if dialog.selections is None: quit()
 # Get the user's input from the dialog.
 selections: Selections = dialog.selections
 ICGCFilePaths = list(selections.getFilePathGroups())[0] # A list of ICGC mutation file paths
+MSIDonorFilePaths = list(selections.getFilePathGroups())[1]
 convertToBed = list(selections.getToggleStates())[0]
 convertToMSIseq = list(selections.getToggleStates())[1]
-if not (convertToBed or convertToMSIseq): raise ValueError("Error: no output format selected.")
+createIndividualDonorFiles = list(selections.getToggleStates())[2]
+separatebyMSI = list(selections.getToggleStates())[3]
+
+
+if not (convertToBed or convertToMSIseq): raise ValueError("Error: No output format selected.")
+if separatebyMSI and not convertToBed: raise ValueError("Error: Cannot separate by MSI without converting to bed.")
+if createIndividualDonorFiles and not convertToBed: raise ValueError("Error: Cannot create individual bed files without converting to bed.")
+
+# If we are separating by microsattelite instability, read all the MSI donors into a dictionary for easy lookup.
+if separatebyMSI: 
+    MSIDonors = dict()
+    for MSIDonorFilePath in MSIDonorFilePaths:
+        with open(MSIDonorFilePath, 'r') as MSIDonorFile:
+            for donor in MSIDonorFile: MSIDonors[donor.strip()] = None
 
 # Run the parser for each ICGC file given.
 for ICGCFilePath in ICGCFilePaths:
@@ -155,42 +250,95 @@ for ICGCFilePath in ICGCFilePaths:
         raise ValueError("Error:  Expected gzipped file.")
     if not "simple_somatic_mutation" in os.path.split(ICGCFilePath)[1]:
         raise ValueError("Error:  Expected file with \"simple_somatic_mutation\" in the name.")
-    
 
-    # Create a directory for intermediate files if it does not already exist...
-    if not os.path.exists(os.path.join(os.path.dirname(ICGCFilePath),"intermediate_files")):
-        os.mkdir(os.path.join(os.path.dirname(ICGCFilePath),"intermediate_files"))
+    # Prepare the file system...
+    fileManager = ICGCParserFileManager(ICGCFilePath, convertToBed, convertToMSIseq, separatebyMSI, createIndividualDonorFiles)
 
-    # Generate paths to output files.
-    mutationGroupName = os.path.split(ICGCFilePath)[1].rsplit('.',3)[-3]
+    # This function handles the writing of mutation data to individual donor files.
+    def writeIndividualDonorFile(donorID, mutations):
 
-    if convertToBed:
-        unsortedBedFilePath = os.path.join(os.path.dirname(ICGCFilePath),"intermediate_files",
-            mutationGroupName+"_unsorted_singlenuc_context.bed")
-        sortedBedFilePath = os.path.join(os.path.dirname(ICGCFilePath),mutationGroupName+"_singlenuc_context.bed")
-    
-    if convertToMSIseq:
-        unsortedMSIseqDataFilePath = os.path.join(os.path.dirname(ICGCFilePath),"intermediate_files",
-            mutationGroupName+"_unsorted_MSIseq_data.bed")
-        sortedMSIseqDataFilePath = os.path.join(os.path.dirname(ICGCFilePath),mutationGroupName+"_MSIseq_data.tsv")
+        # Get the file paths
+        individualDonorBedFilePath = fileManager.getIndividualDonorFilePath(donorID)
+        if separatebyMSI:
+            if donorID in MSIDonors: individualDonorMSBedFilePath = fileManager.getIndividualDonorFilePath(donorID,MSI = True)
+            else: individualDonorMSBedFilePath = fileManager.getIndividualDonorFilePath(donorID,MSI = False)
 
-    # Parse the files!
+        # Open
+        individualDonorBedFile = open(individualDonorBedFilePath,'w')
+        if separatebyMSI: individualDonorMSBedFile = open(individualDonorMSBedFilePath,'w')
+
+        # Write
+        for mutation in mutations:
+
+            if mutation.isValidForBed():
+                individualDonorBedFile.write(mutation.formatForBed() + '\n')
+                if separatebyMSI: individualDonorMSBedFile.write(mutation.formatForBed() + '\n')
+
+        # Close
+        individualDonorBedFile.close()
+        if separatebyMSI: individualDonorMSBedFile.close()
+
+        # Sort
+        subprocess.run(" ".join(("sort","-k1,1","-k2,2n",individualDonorBedFilePath,"-o",individualDonorBedFilePath)), shell = True, check = True)
+        if separatebyMSI: subprocess.run(" ".join(("sort","-k1,1","-k2,2n",individualDonorMSBedFilePath,"-o",individualDonorMSBedFilePath)),
+                                            shell = True, check = True)     
+
+    # Parse the file!
     with gzip.open(ICGCFilePath, 'r') as ICGCFile:
 
-        if convertToBed: unsortedBedFile = open(unsortedBedFilePath, 'w')
-        if convertToMSIseq: unsortedMSIseqDataFile = open(unsortedMSIseqDataFilePath, 'w')
+        # Open the files to write to.
+        if convertToBed: 
+            bedFile = open(fileManager.bedFilePath, 'w')
 
-        for mutation in ICGCIterator(ICGCFile):
-            if convertToBed and mutation.isValidForBed(): unsortedBedFile.write(mutation.formatForBed() + '\n')   
-            if convertToMSIseq: unsortedMSIseqDataFile.write(mutation.formatForMSIseq() + '\n')
+            if separatebyMSI: 
+                MSIBedFile = open(fileManager.MSIBedFilePath, 'w')
+                MSSBedFile = open(fileManager.MSSBedFilePath, 'w')
 
-        if convertToBed: unsortedBedFile.close()
-        if convertToMSIseq: unsortedMSIseqDataFile.close()
+        if convertToMSIseq: MSIseqDataFile = open(fileManager.MSIseqDataFilePath, 'w')
+        
+        iterator = ICGCIterator(ICGCFile) # The object to read through the ICGC data and discard duplicate mutations for each donor.
+        currentDonorID = None # Keeps track of when all of a donor's data has been read.       
 
-    # Sort the mutationData using linux sort, because it's not quite so greedy with memory usage...
+        # Loop through the ICGC data, writing data to the opened files.
+        for mutation in iterator:
+
+            if convertToBed and mutation.isValidForBed(): 
+                bedFile.write(mutation.formatForBed() + '\n')
+
+                if separatebyMSI:
+                    if mutation.donorID in MSIDonors: MSIBedFile.write(mutation.formatForBed() + '\n')
+                    else: MSSBedFile.write(mutation.formatForBed() + '\n')
+
+            if convertToMSIseq: MSIseqDataFile.write(mutation.formatForMSIseq() + '\n')
+
+            # Take these extra steps to stratify data by donorID if it was requested by the user and we have read all of a donor's data.
+            if currentDonorID is None: currentDonorID = mutation.donorID
+            elif createIndividualDonorFiles and mutation.donorID != currentDonorID:
+                writeIndividualDonorFile(currentDonorID, iterator.previousDonorMutations)
+                currentDonorID = mutation.donorID
+
+        # Make sure we don't miss the last donor for individual files!
+        if createIndividualDonorFiles: writeIndividualDonorFile(currentDonorID, iterator.currentDonorMutations.values())
+
+        # Close the files that were opened.
+        if convertToBed: 
+            bedFile.close()
+
+            if separatebyMSI: 
+                MSIBedFile.close()
+                MSSBedFile.close()
+
+        if convertToMSIseq: MSIseqDataFile.close()
+
+    # Sort the mutation data using linux sort, because it's not quite so greedy with memory usage...
     if convertToBed:
         print("Sorting bed data...")
-        subprocess.run(" ".join(("sort","-k1,1","-k2,2n",unsortedBedFilePath,">",sortedBedFilePath)), shell = True, check = True)
+        subprocess.run(" ".join(("sort","-k1,1","-k2,2n",fileManager.bedFilePath,"-o",fileManager.bedFilePath)), shell = True, check = True)
+
+        if separatebyMSI:
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",fileManager.MSIBedFilePath,"-o",fileManager.MSIBedFilePath)), shell = True, check = True)
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",fileManager.MSSBedFilePath,"-o",fileManager.MSSBedFilePath)), shell = True, check = True)
+
     if convertToMSIseq:
         print("Sorting MSIseq data...")
-        subprocess.run(" ".join(("sort","-k5,5","-k2,2n",unsortedMSIseqDataFilePath,">",sortedMSIseqDataFilePath)), shell = True, check = True)
+        subprocess.run(" ".join(("sort","-k5,5","-k2,2n",fileManager.MSIseqDataFilePath,"-o",fileManager.MSIseqDataFilePath)), shell = True, check = True)
