1114003394e0fd868e3d5170181c6fdca3e9bad9
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-05-21 16:33:39 -0700

    Now outputs results to a given RData file

--- R_scripts/RunNucPeriod/RunNucleosomeMutationAnalysis.R
+++ R_scripts/RunNucPeriod/RunNucleosomeMutationAnalysis.R
@@ -7,7 +7,10 @@ selectInputAndRun = function(){
                                 filters = c(c("Tab Separated Files (*.tsv)","Any files"),
                                             c("*.tsv","*.*")), index = 1)
   
-  generateNucPeriodData(inputDataPaths, enforceInputNamingConventions = TRUE)
+  generateNucPeriodData(inputDataPaths, outputFilePath = "nucMutationAnalysis.RData",
+                        enforceInputNamingConventions = TRUE)
+  
+  print("Results outputted to nucMutationAnalysis.RData")
   
 }
 
@@ -15,21 +18,25 @@ selectInputAndRun = function(){
 # Get potential arguments from command line calls and use them to determine what action to take.
 args = commandArgs(trailingOnly = T)
 
-if (length(args) == 1) {
+if (length(args) == 2) {
   
-  # One argument should mean that the script was passed a string of file paths separated by '$'
-  generateNucPeriodData(unlist(strsplit(args[1],'$')), enforceInputNamingConventions = TRUE)
+  # Two arguments should mean that the script was passed a string of input file paths separated by '$'
+  # followed by the path to the output file.
+  generateNucPeriodData(unlist(strsplit(args[1],'$')), args[2], enforceInputNamingConventions = TRUE)
   
-} else if (length(args) == 3) {
+} else if (length(args) == 4) {
   
-  # 3 arguments should mean that script was passed the file paths along with MSI and MSS designations (in that order).
-  generateNucPeriodData(unlist(strsplit(args[1],'$')),unlist(strsplit(args[2],'$')),unlist(strsplit(args[3],'$')))
+  # Four arguments should mean that script was passed the input file paths and the output file path
+  # along with MSI and MSS designations (in that order).
+  generateNucPeriodData(unlist(strsplit(args[1],'$')),args[2],
+                        unlist(strsplit(args[3],'$')),unlist(strsplit(args[4],'$')))
   
 } else if (length(args) == 0) {
   
   selectInputAndRun()
 
 } else {
-  stop("Invalid number of arguments passed.  Expected 1 argument for mutation counts, 3 arguments for
-        mutation counts and MSI/MSS designations or no arguments to select input manually")
+  stop("Invalid number of arguments passed.  Expected 2 argument for mutation counts and output path, 
+        4 arguments for mutation counts, output path, and MSI/MSS designations,
+        or no arguments to select input manually")
 }
