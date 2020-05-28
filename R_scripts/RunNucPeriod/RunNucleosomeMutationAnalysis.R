0fd6cce10b7d8baa8f9a6a01144ea058a8dd8eb7
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-05-27 18:40:14 -0700

    Script now correctly takes input from command line

--- R_scripts/RunNucPeriod/RunNucleosomeMutationAnalysis.R
+++ R_scripts/RunNucPeriod/RunNucleosomeMutationAnalysis.R
@@ -15,28 +15,44 @@ selectInputAndRun = function(){
 }
 
 
-# Get potential arguments from command line calls and use them to determine what action to take.
+# Gets a file path from command line which contains inputs for the generateNucPeriodData function.
+# If there are no command line arguments, the above function is run instead.
 args = commandArgs(trailingOnly = T)
 
-if (length(args) == 2) {
+if (length(args) == 1) {
   
-  # Two arguments should mean that the script was passed a string of input file paths separated by '$'
-  # followed by the path to the output file.
-  generateNucPeriodData(unlist(strsplit(args[1],'$')), args[2], enforceInputNamingConventions = TRUE)
+  # Read in inputs from the given file path.
+  inputFile = file(args[1],'r')
+  inputs = readLines(inputFile)
+  close(inputFile)
   
-} else if (length(args) == 4) {
-  
-  # Four arguments should mean that script was passed the input file paths and the output file path
-  # along with MSI and MSS designations (in that order).
-  generateNucPeriodData(unlist(strsplit(args[1],'$')),args[2],
-                        unlist(strsplit(args[3],'$')),unlist(strsplit(args[4],'$')))
+  # Call the function to generate the NucPeriodData object based on the inputs in the given file.
+  if (length(inputs) == 2) {
+    
+    # Two inputs should mean that the input file contains a string of mutation counts file paths separated by '$'
+    # followed by the path to the output file.
+    generateNucPeriodData(unlist(strsplit(inputs[1],'$',fixed = TRUE)), inputs[2], 
+                          enforceInputNamingConventions = TRUE)
+    
+  } else if (length(inputs) == 4) {
+    
+    # Two inputs should mean that the input file contains the counts file paths and the output file path
+    # along with MSI and MSS designations (in that order).
+    generateNucPeriodData(unlist(strsplit(inputs[1],'$',fixed = TRUE)),inputs[2],
+                          unlist(strsplit(inputs[3],'$',fixed = TRUE)),
+                          unlist(strsplit(inputs[4],'$',fixed = TRUE)),
+                          enforceInputNamingConventions = TRUE)
+    
+  } else {
+    stop("Invalid number of arguments in input file.  Expected 2 argument for mutation counts and output path, or
+          4 arguments for mutation counts, output path, and MSI/MSS designations.")
+  }
   
 } else if (length(args) == 0) {
   
-  selectInputAndRun()
-
 } else {
-  stop("Invalid number of arguments passed.  Expected 2 argument for mutation counts and output path, 
-        4 arguments for mutation counts, output path, and MSI/MSS designations,
+  stop("Invalid number of command line arguments passed.  Expected 1 argument for input data file path, 
         or no arguments to select input manually")
 }
+
+  
