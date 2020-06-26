09d1428d274efd52ca8151850c9b11e924874bb9
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-06-26 12:53:46 -0700

    Updated for new file system
    - Updated naming conventions
    - Tested using metadata class to clean up code and more reliably retrieve nucleosome positioning and genome file information.
    - Tested using generateFilePath function.

--- python_scripts/GenerateNucleosomeMutationBackground.py
+++ python_scripts/GenerateNucleosomeMutationBackground.py
@@ -5,106 +5,54 @@ from TkinterDialog import TkinterDialog, Selections
 import os, subprocess
 from typing import Dict
 from UsefulBioinformaticsFunctions import bedToFasta, reverseCompliment, FastaFileIterator
+from UsefulFileSystemFunctions import getContext, getLinkerDNAAmount, Metadata, generateFilePath
+
 
 # This function takes a bed file of strongly positioned nucleosomes and expands their coordinates to encompass
 # 75 bases on either side of the dyad. (in order to get up to pentanucleotide sequences for positions -73 to 73)
-# Also, if a linker offset is requested, the expansion will be even greater.
-# Returns the file path to the newly expanded file.
-def expandNucleosomeCoordinates(strongPosNucleosomeFilePath, linkerOffset):
+# If a linker offset is requested, the expansion will be even greater to accomodate.
+# The expanded bed file is then used to generate a fasta file of nucleosome sequences.
+# Returns the file path to the fasta file.
+def generateStrongPosNucleosomeFasta(baseNucPosFilePath, genomeFilePath, linkerOffset):
 
-    # Generate a file path for the expanded nucleosome coordinate file.
-    if includeLinker:
-        strongPosNucleosomeExpansionFilePath = strongPosNucleosomeFilePath.rsplit('.',1)[0] + "_expansion+linker.bed"
-    else:
-        strongPosNucleosomeExpansionFilePath = strongPosNucleosomeFilePath.rsplit('.',1)[0] + "_expansion.bed"
+    # Generate a path to the fasta file of strongly positioned nucleosome sequences (Potentially including linker DNA).
+    nucPosFastaFilePath = generateFilePath(directory = os.path.dirname(baseNucPosFilePath),
+                                           dataGroup = os.path.basename(baseNucPosFilePath).rsplit('.',1)[0],
+                                           linkerOffset = linkerOffset,
+                                           fileExtension = ".fa") 
 
-    # Check to see if the expansion file already exists.
-    if os.path.exists(strongPosNucleosomeExpansionFilePath):
-        print ("Expanded nucleosome coordinate file already exists.  Not re-expanding.")
-        return strongPosNucleosomeExpansionFilePath
+    # Make sure the file doesn't already exist.  If it does, we're done!
+    if os.path.exists(nucPosFastaFilePath):
+        print("Found relevant nucleosome fasta file:",os.path.basename(nucPosFastaFilePath))
+        return nucPosFastaFilePath
+    else: print("Nucleosome fasta file not found at: ",nucPosFastaFilePath,"\nGenerating...", sep = '')
 
+    # Generate the expanded file path.
+    expandedNucPosBedFilePath = generateFilePath(directory = os.path.dirname(baseNucPosFilePath),
+                                                 dataGroup = os.path.basename(baseNucPosFilePath).rsplit('.',1)[0],
+                                                 linkerOffset = linkerOffset, dataType = "expanded", fileExtension = ".bed")
+
+    # Expand the bed coordinates.
     print("Expanding nucleosome coordinates...")
-
-    # Open the files.
-    with open(strongPosNucleosomeFilePath,'r') as strongPosNucleosomeFile:
-        with open(strongPosNucleosomeExpansionFilePath, 'w') as strongPosNucleosomeExpansionFile:
+    with open(baseNucPosFilePath,'r') as baseNucPosFile:
+        with open(expandedNucPosBedFilePath, 'w') as expandedNucPosBedFile:
 
             # Write the expanded positions to the new file, one line at a time.
-            for line in strongPosNucleosomeFile:
+            for line in baseNucPosFile:
                 choppedUpLine = line.strip().split('\t')
                 choppedUpLine[1] = str(int(choppedUpLine[1]) - 75 - linkerOffset)
                 choppedUpLine[2] = str(int(choppedUpLine[2]) + 75 + linkerOffset)
 
                 # Write the results to the expansion file as long as it is not before the start of the chromosome.
-                if int(choppedUpLine[1]) != -1: strongPosNucleosomeExpansionFile.write('\t'.join(choppedUpLine) + '\n')
+                if int(choppedUpLine[1]) > -1: expandedNucPosBedFile.write('\t'.join(choppedUpLine) + '\n')
                 else: print("Nucleosome at chromosome", choppedUpLine[0], "with expanded start pos", choppedUpLine[1],
                             "extends into invalid positions.  Skipping.")
+                                            
+    # Convert the expanded bed file to fasta format.
+    print("Converting expanded coordinates to fasta file...")
+    bedToFasta(expandedNucPosBedFilePath,genomeFilePath,nucPosFastaFilePath, includeStrand=False)
 
-    # Return the file path to the newly expanded file.
-    return strongPosNucleosomeExpansionFilePath
-
-
-# The user could have given a nucleosome positioning file in one of several reasonable formats... Parse away!
-def parseStrongPosNucleosomeData(strongPosNucleosomeFilePath,strongPosNucleosomeFastaFilePath,genomeFilePath,linkerOffset):
-
-    # The fasta format should already be formatted correctly, so no further formatting should be necessary.
-    if strongPosNucleosomeFilePath.endswith(".fa"):
-        strongPosNucleosomeFastaFilePath = strongPosNucleosomeFilePath
-        print("Nucleosome positioning data given in fasta format.")
-
-    elif os.path.exists(strongPosNucleosomeFastaFilePath):
-        print("Found fasta file associated with given nucleosome positioning file!") 
-
-    # It's not a fasta file, so check if the given bed (hopefully) file needs to be expanded, then generate the fasta file.
-    else:
-        with open(strongPosNucleosomeFilePath, 'r') as strongPosNucleosomeFile:
-            strongPosNucleosomeFile.readline()
-            choppedUpLine = strongPosNucleosomeFile.readline().strip().split('\t')
-
-            strongPosNucleosomeExpandedFilePath = strongPosNucleosomeFilePath # Assume the file is expanded for now.
-
-            # If it is not actually expanded, expand it!
-            if int(choppedUpLine[2]) - int(choppedUpLine[1]) == 1:
-                print("Given unexpanded nucleosome coordinates.")
-                strongPosNucleosomeExpandedFilePath = expandNucleosomeCoordinates(strongPosNucleosomeFilePath,includeLinker)
-
-            # If it is expanded, but not with/without linker DNA as specified, expand as necessary.
-            elif int(choppedUpLine[2]) - int(choppedUpLine[1]) == 151:
-                print("Given expanded nucleosome coordinates without linker DNA.")
-                if linkerOffset:
-                    print("Need expanded nucleosome coordinates including linker DNA.  Calling expander...")
-                    strongPosNucleosomeFilePath = strongPosNucleosomeFilePath.rsplit("_expansion",1)[0] + ".bed"
-                    strongPosNucleosomeExpandedFilePath = expandNucleosomeCoordinates(strongPosNucleosomeFilePath, includeLinker)
-
-            elif int(choppedUpLine[2]) - int(choppedUpLine[1]) == 211:
-                print("Given expanded nucleosome cooridantes with linker DNA")
-                if not linkerOffset:
-                    print("Need expanded nucleosome coordinates not including linker DNA.  Calling expander...")
-                    strongPosNucleosomeFilePath = strongPosNucleosomeFilePath.rsplit("_expansion",1)[0] + ".bed"
-                    strongPosNucleosomeExpandedFilePath = expandNucleosomeCoordinates(strongPosNucleosomeFilePath, includeLinker)
-            
-            else:
-                raise ValueError("Error: Strongly positioned nucleosome data is not in the expected format.\n" +  
-                    "Each coordinate should contain the central base pair in the dyad only, " + 
-                    "or, exactly 75 bp on either side,\n" +
-                    "or exactly 105 bp on either side (30 bp of linker DNA).")
-
-            # Convert the file to fasta format!
-            print("Converting strongly positioned nucleosome coordinates to fasta file...")
-            bedToFasta(strongPosNucleosomeExpandedFilePath,genomeFilePath,strongPosNucleosomeFastaFilePath, includeStrand=False)
-
-
-# Determines the context used to generate the given mutation background
-def determineBackgroundContext(mutationBackgroundFilePath):
-
-    with open(mutationBackgroundFilePath, 'r') as mutationBackgroundFile:
-
-        # Skip the header line
-        mutationBackgroundFile.readline()
-
-        # Return the length of the context
-        choppedUpLine = mutationBackgroundFile.readline().strip().split('\t')
-        return len(choppedUpLine[0])
+    return nucPosFastaFilePath
 
 
 # Returns a dictionary of background mutation rates for the relevant contexts in the associated genome.
@@ -123,7 +71,7 @@ def getGenomeBackgroundMutationRates(genomeMutationBackgroundFilePath):
 
 
 # This function generates a file of context counts in the genome for each dyad position.
-def generateDyadPosContextCounts(strongPosNucleosomeFastaFilePath, dyadPosContextCountsFilePath, 
+def generateDyadPosContextCounts(nucPosFastaFilePath, dyadPosContextCountsFilePath, 
                                  contextNum, linkerOffset):
 
     # Dictionary of context counts for every dyad position. (Contains a dictionary of either counts for each dyad position)
@@ -136,11 +84,11 @@ def generateDyadPosContextCounts(strongPosNucleosomeFastaFilePath, dyadPosContex
         plusStrandNucleosomeDyadPosContextCounts[dyadPos] = dict()
 
     # Read through the file, adding contexts for every dyad position to the running total in the dictionary
-    with open(strongPosNucleosomeFastaFilePath, 'r') as strongPosNucleosomeFastaFile:
+    with open(nucPosFastaFilePath, 'r') as nucPosFastaFile:
 
         trackedPositionNum = 73*2 + 1 + linkerOffset*2 # How many dyad positions we care about.
 
-        for fastaEntry in FastaFileIterator(strongPosNucleosomeFastaFile):
+        for fastaEntry in FastaFileIterator(nucPosFastaFile):
 
             # Reset dyad position counter
             dyadPos = -73 - linkerOffset
@@ -262,32 +210,22 @@ def generateNucleosomeMutationBackgroundFile(dyadPosContextCountsFilePath, mutat
             nucleosomeMutationBackgroundFile.write(dataRow + '\n')
 
 
-def generateNucleosomeMutationBackground(mutationBackgroundFilePaths, genomeFilePath, 
-                                         strongPosNucleosomeFilePath, includeLinker):
-    
-    # Set the linker offset.
-    if includeLinker: linkerOffset = 30
-    else: linkerOffset = 0
+def generateNucleosomeMutationBackground(mutationBackgroundFilePaths, linkerOffset):
 
     # Whitespace for readability
     print()
-
-    # Generate a path to the fasta file of strongly positioned nucleosome coordinates (Potentially including linker DNA).
-    if (strongPosNucleosomeFilePath.rsplit('.',1)[0].endswith("_expansion") or 
-        strongPosNucleosomeFilePath.rsplit('.',1)[0].endswith("_expansion+linker")):
-        if includeLinker: strongPosNucleosomeFastaFilePath = strongPosNucleosomeFilePath.rsplit("_expansion",1)[0] + "+linker.fa"
-        else: strongPosNucleosomeFastaFilePath = strongPosNucleosomeFilePath.rsplit("_expansion",1)[0] + ".fa"
-    else:
-        if includeLinker: strongPosNucleosomeFastaFilePath = strongPosNucleosomeFilePath.rsplit('.',1)[0] + "+linker.fa"
-        else: strongPosNucleosomeFastaFilePath = strongPosNucleosomeFilePath.rsplit('.',1)[0] + ".fa"
-
-    # Make sure we have a fasta file for strongly positioned nucleosome coordinates
-    parseStrongPosNucleosomeData(strongPosNucleosomeFilePath,strongPosNucleosomeFastaFilePath,genomeFilePath,linkerOffset)
 
     nucleosomeMutationBackgroundFilePaths = list() # A list of paths to the output files generated by the function
 
     # Loop through each given mutation background file path, creating a corresponding nucleosome mutation background for each.
     for mutationBackgroundFilePath in mutationBackgroundFilePaths:
+
+        # Get metadata
+        metadata = Metadata(mutationBackgroundFilePath)
+        genomeFilePath = metadata.getGenomeFilePath()
+
+        # Make sure we have a fasta file for strongly positioned nucleosome coordinates
+        nucPosFastaFilePath = generateStrongPosNucleosomeFasta(metadata.getBaseNucPosFilePath(), genomeFilePath,linkerOffset)
 
         mutationBackgroundFileName = os.path.split(mutationBackgroundFilePath)[1]
         print("\nWorking with file:",mutationBackgroundFileName)
@@ -295,42 +233,34 @@ def generateNucleosomeMutationBackground(mutationBackgroundFilePaths, genomeFile
             raise ValueError("Error, expected file with \"_mutation_background\" in the name.")
 
         # Determine the context of the mutation background file
-        backgroundContextNum = determineBackgroundContext(mutationBackgroundFilePath)
-
-        # Set the name of the type of context being used.
-        if backgroundContextNum == 1: contextText = "singlenuc"
-        elif backgroundContextNum == 3: contextText = "trinuc"
-        elif backgroundContextNum == 5: contextText = "pentanuc"
+        contextNum = getContext(mutationBackgroundFilePath, asInt=True)
+        contextText = getContext(mutationBackgroundFilePath)
 
         print("Given mutation background is in", contextText, "context.")
 
         # Generate the path to the tsv file of dyad position context counts
-        if includeLinker:
-            dyadPosContextCountsFilePath = strongPosNucleosomeFastaFilePath.rsplit(".",1)[0] + "_dyad_pos+linker"
-        else:
-            dyadPosContextCountsFilePath = strongPosNucleosomeFastaFilePath.rsplit(".",1)[0] + "_dyad_pos"
-
-            dyadPosContextCountsFilePath += "_" + contextText + "_counts.tsv"
+        dyadPosContextCountsFilePath = generateFilePath(directory = os.path.dirname(metadata.getBaseNucPosFilePath()),
+                                                        dataGroup = metadata.getNucPosName(),
+                                                        context = contextText, linkerOffset = linkerOffset,
+                                                        dataType = "dyad_pos_counts", fileExtension = ".tsv")
 
         # Make sure we have a tsv file with the appropriate context counts at each dyad position.
         if not os.path.exists(dyadPosContextCountsFilePath): 
             print("Dyad position " + contextText + " counts file not found at",dyadPosContextCountsFilePath)
             print("Generating genome wide dyad position " + contextText + " counts file...")
-            generateDyadPosContextCounts(strongPosNucleosomeFastaFilePath, dyadPosContextCountsFilePath,
-                                        backgroundContextNum, linkerOffset)
+            generateDyadPosContextCounts(nucPosFastaFilePath, dyadPosContextCountsFilePath,
+                                         contextNum, linkerOffset)
 
         # Get some information on the file system and generate file paths for convenience.
         workingDirectory = os.path.dirname(mutationBackgroundFilePath) # The working directory for the current data "group"
 
         # The name of the mutation data set the background pertains to.
-        mutationDataGroup = mutationBackgroundFileName.split("_mutation_background",1)[0]
+        mutationDataGroup = mutationBackgroundFileName.split("_mutation_background",1)[0].rsplit('_',1)[0]
 
         # A path to the final output file.
-        nucleosomeMutationBackgroundFilePath = os.path.join(workingDirectory,mutationDataGroup+"_"+contextText+"_nucleosome_mutation_background")
-        if includeLinker:
-            nucleosomeMutationBackgroundFilePath += "+linker.tsv"
-        else: 
-            nucleosomeMutationBackgroundFilePath += ".tsv"
+        nucleosomeMutationBackgroundFilePath = os.path.join(workingDirectory,mutationDataGroup+"_"+contextText+"_")
+        if linkerOffset > 0: nucleosomeMutationBackgroundFilePath += str(linkerOffset) +"linker+_"
+        nucleosomeMutationBackgroundFilePath += "nucleosome_mutation_background.tsv"
 
         # Generate the nucleosome mutation background file!
         generateNucleosomeMutationBackgroundFile(dyadPosContextCountsFilePath,mutationBackgroundFilePath,
@@ -345,8 +275,6 @@ if __name__ == "__main__":
     #Create the Tkinter UI
     dialog = TkinterDialog(workingDirectory=os.path.join(os.path.dirname(__file__),"..","data"))
     dialog.createMultipleFileSelector("Mutation Background Files:",0,"mutation_background.tsv",("Tab Seperated Values Files",".tsv"))
-    dialog.createFileSelector("Genome Fasta File:",1,("Fasta Files",".fa"))
-    dialog.createFileSelector("Strongly Positioned Nucleosome File:",2,("Bed or Fasta Files",".bed .fa"))
     dialog.createCheckbox("Include linker DNA",3,0,2)
     dialog.createReturnButton(4,0,2)
     dialog.createQuitButton(4,2,2)
@@ -362,9 +290,9 @@ if __name__ == "__main__":
     filePaths = list(selections.getFilePaths())
     mutationBackgroundFilePaths = list(selections.getFilePathGroups())[0] # A list of mutation background file paths
     includeLinker: bool = list(selections.getToggleStates())[0] # Whether or not to include linker DNA on either side of the nucleosomes.
-    genomeFilePath = filePaths[0] # The path to the genome fasta file
-    strongPosNucleosomeFilePath: str = filePaths[1] # The path to the file containing strongly positioned nucleosomes.
   
+    # Set the linker offset.
+    if includeLinker: linkerOffset = 30
+    else: linkerOffset = 0
 
-    generateNucleosomeMutationBackground(mutationBackgroundFilePaths, genomeFilePath, 
-                                         strongPosNucleosomeFilePath, includeLinker)
+    generateNucleosomeMutationBackground(mutationBackgroundFilePaths, linkerOffset)
