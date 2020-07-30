aac3cb18eb0e9245860e5de6dab4adec34ab4347
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-07-29 17:23:36 -0700

    Added verbose parameter to bedToFasta. (Off by default)

--- python_packages/mutperiodpy/mutperiodpy/helper_scripts/UsefulBioinformaticsFunctions.py
+++ python_packages/mutperiodpy/mutperiodpy/helper_scripts/UsefulBioinformaticsFunctions.py
@@ -147,10 +147,11 @@ class FastaFileIterator:
 
 
 def bedToFasta(bedFilePath, genomeFilePath, fastaOutputFilePath, 
-               incorporateBedName = False, includeStrand = True):
+               incorporateBedName = False, includeStrand = True, verbose = False):
     "Uses bedtools to convert a bed file to fasta format."
 
-    print("Calling shell subprocess to use bedtools to generate a fasta file from the given bed file...")
+    if verbose:
+        print("Calling shell subprocess to use bedtools to generate a fasta file from the given bed file...")
 
     optionalParameters = list()
     if includeStrand: optionalParameters.append("-s")
