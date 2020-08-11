c8f19e2fc766115783e319f12d247d92364d22d5
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-08-11 15:44:27 -0700

    Manager now sorts output files.

--- python_packages/mutperiodpy/mutperiodpy/input_parsing/WriteManager.py
+++ python_packages/mutperiodpy/mutperiodpy/input_parsing/WriteManager.py
@@ -1,7 +1,7 @@
 # This script contains a class for managing the writing of input data to various stratified data files.
 # Once set up, it only needs to be passed data one line at a time.
 
-import os
+import os, subprocess
 from typing import IO
 from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import (Metadata, checkDirs, dataTypes, generateFilePath,
                                                                   generateMetadata, getIsolatedParentDir)
@@ -17,9 +17,9 @@ class WriteManager:
         self.rootMetadata = Metadata(self.rootDataDir)
 
         # create and open the output file in the same directory as the root data.
-        rootOutputFilePath = generateFilePath(directory = self.rootDataDir, dataGroup = self.rootMetadata.dataGroupName,
-                                              context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
-        self.rootOutputFile = open(rootOutputFilePath, 'w')
+        self.rootOutputFilePath = generateFilePath(directory = self.rootDataDir, dataGroup = self.rootMetadata.dataGroupName,
+                                                   context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
+        self.rootOutputFile = open(self.rootOutputFilePath, 'w')
 
         # By default, all other write options are off unless otherwise specified.
         self.stratifyByIndividualCohorts = False
@@ -32,8 +32,8 @@ class WriteManager:
     
     def __exit__(self, type, value, tb):
 
-        # Close all opened files.
-        self.cleanup()
+        # Close all opened files and sort aggregate files.
+        self.cleanupAndSort()
 
 
     # Prepares the manager to separate data by cohort.
@@ -53,7 +53,11 @@ class WriteManager:
     # Open a new individual cohort file for writing (and close the last one, if applicable.)
     def setUpNewIndividualCohort(self, cohortID):
 
-        if self.currentIndividualCohortFile is not None: self.currentIndividualCohortFile.close()
+        # If this isn't the first opened cohort file, close and sort the last one.
+        if self.currentIndividualCohortFile is not None: 
+            self.currentIndividualCohortFile.close()
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.individualCohortFilePath,"-o",self.individualCohortFilePath)), 
+                           shell = True, check = True)
 
         # Make sure this is actually a new cohort.
         if cohortID in self.completedIndividualCohorts:
@@ -77,9 +81,9 @@ class WriteManager:
 
 
         # Generate the file path and metadata file and open the file for writing.
-        individualCohortFilePath = generateFilePath(directory = individualCohortDirectory, dataGroup = individualCohortDataGroup, 
-                                                    context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
-        self.currentIndividualCohortFile = open(individualCohortFilePath, 'w')
+        self.individualCohortFilePath = generateFilePath(directory = individualCohortDirectory, dataGroup = individualCohortDataGroup, 
+                                                         context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
+        self.currentIndividualCohortFile = open(self.individualCohortFilePath, 'w')
         generateMetadata(individualCohortDataGroup, self.rootMetadata.genomeName, self.rootMetadata.nucPosName,
                          os.path.join("..",self.rootMetadata.localParentDataPath), individualCohortDirectory, *cohortMembership)
 
@@ -103,19 +107,19 @@ class WriteManager:
         generateMetadata("MSI_" + self.rootMetadata.dataGroupName, self.rootMetadata.genomeName, self.rootMetadata.nucPosName,
                          os.path.join('..','..',self.rootMetadata.localParentDataPath), aggregateMSIDirectory, "MSI")
 
-        aggregateMSSFilePath = generateFilePath(directory = aggregateMSSDirectory, dataGroup = "MSS_" + self.rootMetadata.dataGroupName,
-                                                context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
-        self.aggregateMSSFile = open(aggregateMSSFilePath, 'w')
-        aggregateMSIFilePath = generateFilePath(directory = aggregateMSIDirectory, dataGroup = "MSI_" + self.rootMetadata.dataGroupName,
-                                                context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
-        self.aggregateMSIFile = open(aggregateMSIFilePath, 'w')
+        self.aggregateMSSFilePath = generateFilePath(directory = aggregateMSSDirectory, dataGroup = "MSS_" + self.rootMetadata.dataGroupName,
+                                                     context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
+        self.aggregateMSSFile = open(self.aggregateMSSFilePath, 'w')
+        self.aggregateMSIFilePath = generateFilePath(directory = aggregateMSIDirectory, dataGroup = "MSI_" + self.rootMetadata.dataGroupName,
+                                                     context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
+        self.aggregateMSIFile = open(self.aggregateMSIFilePath, 'w')
 
         # Set up the MSIIdentifier to be returned.
         intermediateFilesDir = os.path.join(self.rootDataDir,"intermediate_files")
         checkDirs(intermediateFilesDir)
         MSISeqInputDataFilePath = generateFilePath(directory = intermediateFilesDir, dataGroup = self.rootMetadata.dataGroupName,
-                                                   dataType = "MSISeq_data", fileExtension = ".txt")
-        self.MSICohortsFilePath = generateFilePath(directory = self.rootIndividualCohortsDirectory, dataGroup = self.rootMetadata.dataGroupName, 
+                                                   dataType = "MSISeq_data", fileExtension = ".tsv")
+        self.MSICohortsFilePath = generateFilePath(directory = aggregateMSDirectory, dataGroup = self.rootMetadata.dataGroupName, 
                                                    dataType = "MSI_cohorts", fileExtension = ".txt")
 
         self.myMSIIdentifier = MSIIdentifier(MSISeqInputDataFilePath, self.MSICohortsFilePath)
@@ -128,7 +132,11 @@ class WriteManager:
     # Writes the given data to all the relevant files based on how the manager was set up.
     def writeData(self, chromosome, startPos, endPos, mutFrom, alteration, strand, cohortID = '.'):
 
-        outputLine = '\t'.join((chromosome, startPos, endPos, mutFrom, alteration, strand)) + '\n'
+        # Make sure we were given an SNP.  (Right now, that's the only acceptable data format for the pipeline.)
+        # Then, format it for output.
+        if mutFrom.upper() in ('A','C','G','T') and alteration.upper() in ('A','C','G','T'):
+            outputLine = '\t'.join((chromosome, startPos, endPos, mutFrom, mutFrom + ">" + alteration, strand)) + '\n'
+        else: return
 
         # Write data to the root output file.
         self.rootOutputFile.write(outputLine)
@@ -152,7 +160,7 @@ class WriteManager:
 
         # TODO: Write to signature designations if it was set up.
 
-        # Write to individual cohorts as desired.
+        # Write to individual cohorts if desired.
         if self.stratifyByIndividualCohorts and cohortID != '.':
             
             # Check to see if we've reached a new cohortID .
@@ -165,15 +173,23 @@ class WriteManager:
 
 
     # Closes open files to clean up the class after it's done being used.
-    def cleanup(self):
+    def cleanupAndSort(self):
 
         self.rootOutputFile.close()
+        subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.rootOutputFilePath,"-o",self.rootOutputFilePath)),
+                       shell = True, check = True)
 
         if self.stratifyByMS:
             self.aggregateMSIFile.close()
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.aggregateMSIFilePath,"-o",self.aggregateMSIFilePath)), 
+                           shell = True, check = True)
             self.aggregateMSSFile.close()
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.aggregateMSSFilePath,"-o",self.aggregateMSSFilePath)),
+                           shell = True, check = True)
 
         # TODO: Close mutsig files.
 
         if self.stratifyByIndividualCohorts and self.currentIndividualCohortFile is not None:
             self.currentIndividualCohortFile.close()
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.individualCohortFilePath,"-o",self.individualCohortFilePath)), 
+                           shell = True, check = True)
