174332b3239f1879cf8f2e1db76b5bc6e438f200
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-08-18 17:44:24 -0700

    Added stratification by mutation signature

--- python_packages/mutperiodpy/mutperiodpy/input_parsing/WriteManager.py
+++ python_packages/mutperiodpy/mutperiodpy/input_parsing/WriteManager.py
@@ -6,6 +6,7 @@ from typing import IO
 from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import (Metadata, checkDirs, dataTypes, generateFilePath,
                                                                   generateMetadata, getIsolatedParentDir)
 from mutperiodpy.input_parsing.IdentifyMSI import MSIIdentifier
+from mutperiodpy.input_parsing.IdentifyMutSigs import MutSigIdentifier
 
 
 class WriteManager:
@@ -24,6 +25,7 @@ class WriteManager:
         # By default, all other write options are off unless otherwise specified.
         self.stratifyByIndividualCohorts = False
         self.stratifyByMS = False
+        self.stratifyByMutSig = False
         self.stratifyBySignature = False
 
 
@@ -48,44 +50,6 @@ class WriteManager:
         # Create the directory.
         self.rootIndividualCohortsDirectory = os.path.join(self.rootMetadata.directory,"individual_cohorts")
         checkDirs(self.rootIndividualCohortsDirectory)
-
-
-    # Open a new individual cohort file for writing (and close the last one, if applicable.)
-    def setUpNewIndividualCohort(self, cohortID):
-
-        # If this isn't the first opened cohort file, close and sort the last one.
-        if self.currentIndividualCohortFile is not None: 
-            self.currentIndividualCohortFile.close()
-            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.individualCohortFilePath,"-o",self.individualCohortFilePath)), 
-                           shell = True, check = True)
-
-        # Make sure this is actually a new cohort.
-        if cohortID in self.completedIndividualCohorts:
-            raise ValueError("The cohort " + cohortID + " was encountered in more than one distinct block of data.")
-        else:
-            self.currentIndividualCohortID = cohortID
-
-        individualCohortDirectory = os.path.join(self.rootIndividualCohortsDirectory,self.currentIndividualCohortID)
-        individualCohortDataGroup = self.currentIndividualCohortID + "_" + self.rootMetadata.dataGroupName
-
-        checkDirs(individualCohortDirectory)
-
-        # Determine which other set up "umbrella" cohorts this cohort belongs to.
-        cohortMembership = [self.currentIndividualCohortID,]
-        if self.stratifyByMS:
-            if self.currentIndividualCohortID in self.MSICohorts:
-                cohortMembership.append("MSI")
-            else:
-                cohortMembership.append("MSS")
-        # TODO: determine mutsig membership if requested
-
-
-        # Generate the file path and metadata file and open the file for writing.
-        self.individualCohortFilePath = generateFilePath(directory = individualCohortDirectory, dataGroup = individualCohortDataGroup, 
-                                                         context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
-        self.currentIndividualCohortFile = open(self.individualCohortFilePath, 'w')
-        generateMetadata(individualCohortDataGroup, self.rootMetadata.genomeName, self.rootMetadata.nucPosName,
-                         os.path.join("..",self.rootMetadata.localParentDataPath), individualCohortDirectory, *cohortMembership)
 
 
     # Prepares the manager to separate cohorts by microsatellite stability.
@@ -126,7 +90,89 @@ class WriteManager:
         return(self.myMSIIdentifier)
 
 
-    # TODO: set up signature stratification
+    # Prepares the manager to separate cohorts by microsatellite stability.
+    # Returns a MutSigIdentifier object to be "completed" by the function caller.
+    def setUpForMutSigStratification(self) -> MutSigIdentifier:
+
+        self.stratifyByMutSig = True
+        self.mutSigDesignations = dict() # A dictionary of the mutation signatures assigned to each cohort.
+
+        mutSigs = ["1A","1B"] + [str(x) for x in list(range(2,22))] + ["R1","R2","R3","U1","U2"]
+
+        # Create the necessary directories, file paths, and metadata.
+        parentMutSigDirectory = os.path.join(self.rootMetadata.directory, "mut_sig_analysis")
+        self.mutSigFilePaths = dict()
+        self.mutSigFiles = dict()
+
+        for mutSig in mutSigs:
+
+            thisMutSigDataGroup = "mut_sig_" + mutSig + '_' + self.rootMetadata.dataGroupName
+
+            # Directory
+            thisMutSigDirectory = os.path.join(parentMutSigDirectory,mutSig)
+            checkDirs(thisMutSigDirectory)
+
+            # Metadata
+            generateMetadata(thisMutSigDataGroup, self.rootMetadata.genomeName, self.rootMetadata.nucPosName,
+                             os.path.join('..','..',self.rootMetadata.localParentDataPath), thisMutSigDirectory, "mutSig" + mutSig)
+
+            # File path
+            self.mutSigFilePaths[mutSig] = generateFilePath(directory = thisMutSigDirectory, dataGroup = thisMutSigDataGroup, 
+                                                            context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
+            self.mutSigFiles[mutSig] = open(self.mutSigFilePaths[mutSig], 'w')
+
+        # Set up the MutSigIdentifier object to be returned.
+        intermediateFilesDir = os.path.join(self.rootDataDir,"intermediate_files")
+        checkDirs(intermediateFilesDir)
+
+        deconstructSigsInputDataFilePath = generateFilePath(directory = intermediateFilesDir, dataGroup = self.rootMetadata.dataGroupName,
+                                                            dataType = "deconstructSigs_data", fileExtension = ".tsv")
+        self.mutSigDesignationsFilePath = generateFilePath(directory = parentMutSigDirectory, dataGroup = self.rootMetadata.dataGroupName, 
+                                                          dataType = "mut_sig_assignments", fileExtension = ".tsv")
+
+        self.mutSigIdentifier = MutSigIdentifier(deconstructSigsInputDataFilePath, self.mutSigDesignationsFilePath)
+        return(self.mutSigIdentifier)
+
+
+    # Open a new individual cohort file for writing (and close the last one, if applicable.)
+    def setUpNewIndividualCohort(self, cohortID):
+
+        # If this isn't the first opened cohort file, close and sort the last one.
+        if self.currentIndividualCohortFile is not None: 
+            self.currentIndividualCohortFile.close()
+            subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.individualCohortFilePath,"-o",self.individualCohortFilePath)), 
+                           shell = True, check = True)
+
+        # Make sure this is actually a new cohort.
+        if cohortID in self.completedIndividualCohorts:
+            raise ValueError("The cohort " + cohortID + " was encountered in more than one distinct block of data.")
+        else:
+            self.currentIndividualCohortID = cohortID
+
+        individualCohortDirectory = os.path.join(self.rootIndividualCohortsDirectory,self.currentIndividualCohortID)
+        individualCohortDataGroup = self.currentIndividualCohortID + "_" + self.rootMetadata.dataGroupName
+
+        checkDirs(individualCohortDirectory)
+
+        # Determine which other set up "umbrella" cohorts this cohort belongs to.
+        cohortMembership = [self.currentIndividualCohortID,]
+        if self.stratifyByMS:
+            if self.currentIndividualCohortID in self.MSICohorts:
+                cohortMembership.append("MSI")
+            else:
+                cohortMembership.append("MSS")
+        if self.stratifyByMutSig:
+            if self.currentIndividualCohortID in self.mutSigDesignations:
+                for mutSig in self.mutSigDesignations[self.currentIndividualCohortID]:
+                    cohortMembership.append("mut_sig_" + mutSig)
+                
+
+        # Generate the file path and metadata file and open the file for writing.
+        self.individualCohortFilePath = generateFilePath(directory = individualCohortDirectory, dataGroup = individualCohortDataGroup, 
+                                                         context = "singlenuc", dataType = dataTypes.mutations, fileExtension = ".bed")
+        self.currentIndividualCohortFile = open(self.individualCohortFilePath, 'w')
+        generateMetadata(individualCohortDataGroup, self.rootMetadata.genomeName, self.rootMetadata.nucPosName,
+                         os.path.join("..",self.rootMetadata.localParentDataPath), individualCohortDirectory, *cohortMembership)
 
 
     # Writes the given data to all the relevant files based on how the manager was set up.
@@ -158,7 +204,30 @@ class WriteManager:
             else:
                 self.aggregateMSSFile.write(outputLine)
 
-        # TODO: Write to signature designations if it was set up.
+        # Write to signature designations if it was set up.
+        if self.stratifyByMutSig and cohortID != '.':
+
+            # Propogate the mutSigDesignations dictionary if it hasn't been.
+            if len(self.mutSigDesignations) == 0:
+                if not self.mutSigIdentifier.mutSigsIdentified:
+                    raise ValueError("MutSigIdentifier protocol was never completed.")
+                else:
+
+                    with open(self.mutSigDesignationsFilePath, 'r') as mutSigDesignationsFile:
+                        for line in mutSigDesignationsFile:
+
+                            mutSigsCohortID = str(line).strip().split('\t')[0]
+                            mutSigs = str(line).strip().split('\t')[1].split(',')
+
+                            if mutSigs[0] == "None": continue
+                            else: self.mutSigDesignations[mutSigsCohortID] = mutSigs
+
+            # Write the data to its appropriate mutsig file(s) if its cohort has mutsig designations.
+            if cohortID in self.mutSigDesignations:
+                
+                for mutSig in self.mutSigDesignations[cohortID]:
+                    self.mutSigFiles[mutSig].write(outputLine)
+
 
         # Write to individual cohorts if desired.
         if self.stratifyByIndividualCohorts and cohortID != '.':
@@ -187,7 +256,11 @@ class WriteManager:
             subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.aggregateMSSFilePath,"-o",self.aggregateMSSFilePath)),
                            shell = True, check = True)
 
-        # TODO: Close mutsig files.
+        if self.stratifyByMutSig:
+            for mutSig in self.mutSigFiles:
+                self.mutSigFiles[mutSig].close()
+                subprocess.run(" ".join(("sort","-k1,1","-k2,2n",self.mutSigFilePaths[mutSig], "-o",self.mutSigFilePaths[mutSig])), 
+                               shell = True, check = True)
 
         if self.stratifyByIndividualCohorts and self.currentIndividualCohortFile is not None:
             self.currentIndividualCohortFile.close()
