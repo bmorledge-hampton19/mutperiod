d4c7a6104ba4af9450692e79f5a52aee7fa82bb2
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-08-18 17:39:43 -0700

    Create GetMutSigs.R
    Calls the corresponding NucPeriod script when passed arguments from the command line.

---
+++ R_packages/RunNucPeriod/GetMutSigs.R
@@ -0,0 +1,27 @@
+library(NucPeriod)
+library(data.table)
+
+selectInputAndRun = function(){
+  
+  # Get the input data from the user.
+  inputDataPath = choose.files(multi = FALSE, caption = "Select MutSig Input Data", index = 1,
+                               filters = c(c("Tab Separated Files (*.tsv)","Any files"),c("*.tsv","*.*")))
+  
+  fwrite(getMutSigs(inputDataPath), "output.tsv", sep = '\t')
+  
+}
+
+# Get potential arguments from command line calls and use them to determine what action to take.
+args = commandArgs(trailingOnly = T)
+if (length(args) == 0) {
+  selectInputAndRun()
+} else if (length(args) == 2) {
+  fwrite(getMutSigs(args[1]), args[2], sep = '\t')
+} else {
+  stop("Invalid number of arguments passed.  Expected 2 arguments for deconstructSigs input data path
+       and output data path or no arguments to select input manually")
+}
+
+
+
+
