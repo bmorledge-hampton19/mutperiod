9d4792401a72fe184005eafcf465ce5c218a515c
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-08-20 13:18:34 -0700

    Small bugfix

--- python_packages/mutperiodpy/mutperiodpy/helper_scripts/UsefulFileSystemFunctions.py
+++ python_packages/mutperiodpy/mutperiodpy/helper_scripts/UsefulFileSystemFunctions.py
@@ -57,7 +57,7 @@ def getFilesInDirectory(directory,validEnding, *additionalValidEndings):
 # Returns just the name of the first directory above a given path. (e.g. test/file/path.txt would return "file")
 def getIsolatedParentDir(filePath: str, isDir = False):
 
-    if not os.path.isdir() == isDir:
+    if not os.path.isdir(filePath) == isDir:
         if isDir: raise ValueError("Expected directory path, but received the file path: " + filePath)
         else: raise ValueError("Expected file path, but received the directory path: " + filePath)
 
