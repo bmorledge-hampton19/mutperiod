383696c4bd61598049a1499483e2262e0729b515
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-09-02 13:46:13 -0700

    Removed assymetry analysis code

--- R_packages/NucPeriod/R/NucleosomeMutationAnalysis.R
+++ R_packages/NucPeriod/R/NucleosomeMutationAnalysis.R
@@ -56,15 +56,6 @@ generateNucPeriodData = function(mutationCountsFilePaths, outputFilePath,
   periodicityPValues = numeric(length(validFilePaths))
   periodicitySNRs = numeric(length(validFilePaths))
 
-  # generalAsymmetryTValue = numeric(length(validFilePaths))
-  # generalAsymmetryPValue = numeric(length(validFilePaths))
-  #
-  # peakAsymmetryTValue = numeric(length(validFilePaths))
-  # peakAsymmetryPValue = numeric(length(validFilePaths))
-  #
-  # valleyAsymmetryTValue = numeric(length(validFilePaths))
-  # valleyAsymmetryPValue = numeric(length(validFilePaths))
-
   for (i in 1:length(validFilePaths)) {
 
     # Make sure that if no files validated, no analysis attempts to run.
@@ -112,59 +103,11 @@ generateNucPeriodData = function(mutationCountsFilePaths, outputFilePath,
                           | lombResult$scanned > lombResult$peak.at[1] + 0.5)
     periodicitySNRs[i] = lombResult$peak / median(lombResult$power[noiseBooleanVector])
 
-
-    # ##### Asymmetry Analysis ##### ## Obselete.  Will maybe revisit?
-    #
-    # print("Running asymmetry analysis")
-    #
-    # # Run asymmetry analysis on the aligned strand counts.
-    # generalAsymmetryResult =
-    #   t.test(nucleosomeCountsData$Normalized_Aligned_Strands[(74-dyadPosCutoff):73],
-    #          rev(nucleosomeCountsData$Normalized_Aligned_Strands[75:(74+dyadPosCutoff)]), paired = T)
-    # generalAsymmetryTValue[i] = generalAsymmetryResult$statistic
-    # generalAsymmetryPValue[i] = generalAsymmetryResult$p.value
-    #
-    # # Run asymmetry analysis on the peaks and valleys of the aligned strands.
-    # # If there is not enough data to derive peaks and valleys (e.g. for individual donors)
-    # # the result will be NA.
-    # peakAsymmetryResult = runExtremeAnalysisSuite(nucleosomeCountsData$Normalized_Aligned_Strands,
-    #                                               dyadPosCutoff = dyadPosCutoff)
-    # if (any(!is.na(peakAsymmetryResult))) {
-    #   peakAsymmetryTValue[i] = peakAsymmetryResult$statistic
-    #   peakAsymmetryPValue[i] = peakAsymmetryResult$p.value
-    # } else {
-    #   peakAsymmetryTValue[i] = NA
-    #   peakAsymmetryPValue[i] = NA
-    # }
-    #
-    # valleyAsymmetryResult = runExtremeAnalysisSuite(nucleosomeCountsData$Normalized_Aligned_Strands,
-    #                                                 maxes = FALSE, dyadPosCutoff = dyadPosCutoff)
-    # if (any(!is.na(valleyAsymmetryResult))) {
-    #   valleyAsymmetryTValue[i] = valleyAsymmetryResult$statistic
-    #   valleyAsymmetryPValue[i] = valleyAsymmetryResult$p.value
-    # } else {
-    #   valleyAsymmetryTValue[i] = NA
-    #   valleyAsymmetryPValue[i] = NA
-    # }
-
-    # # Negative controls for asymmetry analysis on peaks and valleys
-    # runExtremeAnalysisSuite(normalizedData$Normalized_Both_Strands, dyadPosCutof = 68)
-    # runExtremeAnalysisSuite(normalizedData$Normalized_Both_Strands, maxes = FALSE, dyadPosCutoff = 68)
-
   }
 
   # Create data.tables for all the results.
   periodicityResults = data.table::data.table(Data_Set=validDataSetNames,Peak_Periodicity=peakPeriodicities,
                                               PValue=periodicityPValues,SNR=periodicitySNRs)
-
-  ### Obselete
-  # asymmetryResults = data.table::data.table(Data_Set=validDataSetNames,
-  #                                           General_Asymmetry_TValue = generalAsymmetryTValue,
-  #                                           General_Asymmetry_PValue = generalAsymmetryPValue,
-  #                                           Peak_Asymmetry_TValue = peakAsymmetryTValue,
-  #                                           Peak_Asymmetry_PValue = peakAsymmetryPValue,
-  #                                           Valley_Asymmetry_TValue = valleyAsymmetryTValue,
-  #                                           Valley_Asymmetry_PValue = valleyAsymmetryPValue)
 
   # Run the SNR wilcoxin's test if necessary.
   if (compareMS) {
