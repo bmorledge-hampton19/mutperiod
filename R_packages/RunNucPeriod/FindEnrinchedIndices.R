7b2ce12b909fe25fab5fdc3b36ec256769a720e9
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-09-16 16:51:33 -0700

    Massive Overhaul
    Basically just me messing around with apply functions vs. for loops in R to reconstruct this script.  I'm not sure when I'll ever use this again, and it's pretty specialized already, so if you're reading this, good luck.

--- R_packages/RunNucPeriod/FindEnrinchedIndices.R
+++ R_packages/RunNucPeriod/FindEnrinchedIndices.R
@@ -11,26 +11,72 @@ getBaseIndexFrequency = function(sequences, positionFromEnd = NA, positionFromSt
   }
   
   if (!is.na(positionFromEnd)) {
-    bases = invisible(sapply(sequences, function(x) substr(x, nchar(x)-positionFromEnd+1, 
-                                                           nchar(x)-positionFromEnd+1)))
+    bases = sapply(sequences, function(sequence) {
+      
+      sequenceLength = nchar(sequence)
+      if (sequenceLength < positionFromEnd) {
+        stop(paste0("Desired position (",positionFromEnd," from end) ",
+                    "exceeds sequence length (",sequenceLength,")"))
+      }
+             
+      return(substr(sequence, sequenceLength-positionFromEnd+1, sequenceLength-positionFromEnd+1))
+      
+    })
+    
   } else if (!is.na(positionFromStart)) {
-    bases = invisible(sapply(sequences, function(x) substr(x, positionFromStart+1, 
-                                                           positionFromStart+1)))
+    bases = sapply(sequences, function(sequence) {
+      
+      sequenceLength = nchar(sequence)
+      if (sequenceLength < positionFromStart) {
+        stop(paste0("Desired position (",positionFromStart," from start) ",
+                    "exceeds sequence length (",sequenceLength,")"))
+      }
+      
+      return(substr(sequence, positionFromStart+1, positionFromStart+1))
+      
+    })
   }
-    return(table(factor(bases, c("A","C","G","T")))/length(sequences))
+  
+    countsTable = table(factor(bases, c("A","C","G","T")))
+    return(countsTable/sum(countsTable))
   
 }
 
-# Prints the position of the maximum value of a given base and a vector of frequencies (ordered).
-getMaxFrequencyPosition = function(base, frequencies, startOrEnd = "end") {
+# Calculates the base frequencies for a given set of sequences, at given values, 
+# and return the resulting baseFrequency table.
+calculateBaseFrequencyTable = function(sequences, fromStartValues, fromEndValues) {
   
-  maxValue = max(frequencies)
-  maxPosition = match(maxValue,frequencies)
-  print(paste0("Base ",base," is most common at position ",maxPosition, 
-               " from the ",startOrEnd, " with a frequency of ",maxValue,"."))
+  baseFrequencies = data.table(base = c("A","C","G","T"))
+  
+  for (i in fromStartValues) { 
+    print(paste0("Calculating base frequency at position ",i," base(s) from start."))
+    baseFrequencies[,paste0(i,"_From_Start") := getBaseIndexFrequency(sequences, positionFromStart = i)]
+  }
+    
+  
+  for (i in fromEndValues) { 
+    print(paste0("Calculating base frequency at position ",i," base(s) from end."))
+    baseFrequencies[,paste0(i,"_From_End") := getBaseIndexFrequency(sequences, positionFromEnd = i)]
+  }
+  
+  return(baseFrequencies)
   
 }
 
+# Returns, as text, the position of the maximum frequency value of a given base in a base frequency table
+# followed by the frequency itself.
+getMaxFrequencyInfo = function(thisBase, frequencyTable) {
+  
+  relevantFrequencies = frequencyTable[base == thisBase, -1]
+  maxValue = max(relevantFrequencies)
+  maxFrequencyLocation = colnames(relevantFrequencies)[match(maxValue,relevantFrequencies)]
+  
+  return(list(thisBase, maxValue, maxFrequencyLocation))
+  
+}
+
+
+sequenceLengthRange = 10:40
 
 # Read in the fasta file
 fastaFile = file.choose()
@@ -38,18 +84,45 @@ print(paste0("Reading from ",basename(fastaFile),"..."))
 fastaTable = fread(file = fastaFile, header = FALSE, col.names = "Sequences")
 fastaTable = fastaTable[!startsWith(Sequences,">")]
 
-# Count the occurences of each base at a specific position from the end of each sequence.
-baseFrequencies = data.table(bases = c("A","C","G","T"))
-print("Calculating base frequencies at positions up to 12 from the end of each sequence...")
-invisible(sapply(c(1:12), function(x) baseFrequencies[,paste0(x,"_From_End") := 
-                                                      getBaseIndexFrequency(fastaTable$Sequences,
-                                                                            positionFromEnd = x)]))
-invisible(sapply(c(1:12), function(x) baseFrequencies[,paste0(x,"_From_Start") := 
-                                                      getBaseIndexFrequency(fastaTable$Sequences,
-                                                                            positionFromStart = x)]))
+# Split the fasta sequences based on length
+print("Splitting fasta sequences by length...")
+fastaTableSequenceLengths = nchar(fastaTable$Sequences)
 
-# Print the maxima positions for each base.
-invisible(sapply(baseFrequencies$bases, function(x) getMaxFrequencyPosition(x, baseFrequencies[bases == x,2:13],
-                                                                            "end")))
-invisible(sapply(baseFrequencies$bases, function(x) getMaxFrequencyPosition(x, baseFrequencies[bases == x,14:25],
-                                                                            "start")))
+sequencesByLength = sapply(sequenceLengthRange, function(i) {
+  print(paste0("Getting sequences of Length ",i,"..."))
+  return(fastaTable$Sequences[fastaTableSequenceLengths == i])
+})
+names(sequencesByLength) = as.character(sequenceLengthRange)
+
+# Calculate a base frequency table for each sequence length.
+print("Calculating base frequency table for each sequence length...")
+baseFrequencyTables = vector("list", length(sequenceLengthRange))
+names(baseFrequencyTables) = as.character(sequenceLengthRange)
+for (i in sequenceLengthRange) {
+  print(paste0("Calculating base frequency table for sequence length of ",i,"..."))
+  baseFrequencyTables[[as.character(i)]] = 
+    calculateBaseFrequencyTable(sequencesByLength[[as.character(i)]], 
+                                fromStartValues = numeric(), fromEndValues = 1:10)
+}
+
+# Print the position of the maximum frequency value for G in each frequency table.
+maxValues = numeric(length(sequenceLengthRange))
+names(maxValues) = as.character(sequenceLengthRange)
+maxValueLocations = character(length(sequenceLengthRange))
+names(maxValueLocations) = as.character(sequenceLengthRange)
+
+for (i in sequenceLengthRange) {
+  
+  maxFrequencyInfo = getMaxFrequencyInfo('G', baseFrequencyTables[[as.character(i)]])
+  maxValues[as.character(i)] = maxFrequencyInfo[[2]]
+  maxValueLocations[as.character(i)] = maxFrequencyInfo[[3]]
+  
+  print(paste0("For fragments of length ",i, 
+               ", maximum frequency of guanine is at position ", 
+               maxValueLocations[as.character(i)], " with a frequency of ", 
+               maxValues[as.character(i)], '.'))
+}
+
+maxFrequencyInfoTable = data.table(Sequence_Length = sequenceLengthRange,
+                                   Max_Frequency = maxValues,
+                                   Max_Frequency_Location = maxValueLocations)
