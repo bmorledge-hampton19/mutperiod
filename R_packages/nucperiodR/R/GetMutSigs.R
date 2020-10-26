2b5a3d2d33f703425e65cd4168c610c21023d4e5
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-10-26 16:04:00 -0700

    Changed how the dominant sig is found.
    If the threshold is set to NA, the signature with the highest proportion is chosen.  (If multiple signatures tie for the highest position, they are both included.)

--- R_packages/nucperiodR/R/GetMutSigs.R
+++ R_packages/nucperiodR/R/GetMutSigs.R
@@ -15,12 +15,18 @@ getMutSigs = function(inputFilePath) {
   # Function which returns the signatures for a given sample ID that are at least at the given threshold.
   # If multiple signatures meet the threshold, they are returned as a comma separated string.
   # If no signatures meet the threshold, "None" is returned instead.
-  findDominantSigs = function(sampleID, threshold = .5) {
+  # Alternatively, the threshold may be set to NA, in which case the signature with the highest proportion
+  # is chosen.  (If multiple signatures tie for the highest position, they are both included.)
+  findDominantSigs = function(sampleID, threshold = NA) {
 
     baseMutSigs = deconstructSigs::whichSignatures(trinucCounts,
                                                    signatures.ref = deconstructSigs::signatures.nature2013,
                                                    contexts.needed = T, sample.id = sampleID)
-    validMutSigs = names(baseMutSigs$weights)[baseMutSigs$weights >= threshold]
+    if (is.na(threshold)) {
+      validMutSigs = names(baseMutSigs$weights)[baseMutSigs$weights == max(baseMutSigs$weights)]
+    } else {
+      validMutSigs = names(baseMutSigs$weights)[baseMutSigs$weights >= threshold]
+    }
     validMutSigs = sapply(validMutSigs, function(x) strsplit(x, '.', fixed = TRUE)[[1]][2])
     validMutSigs = paste(validMutSigs, collapse = ',')
 
