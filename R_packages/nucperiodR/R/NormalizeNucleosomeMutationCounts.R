504a7adaa7d0d2b34811cddde12e8dd7acb9a842
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2020-11-04 18:15:09 -0800

    Update R normalization script for custom background and scaling..
    - Script is now robust to custom background counts files (which look like raw counts files because that's what they are) as input.
    - Scales final normalized count values by the ratio of total background:raw counts, so the average of all counts should always be one.  (That's how that works, right?)

--- R_packages/nucperiodR/R/NormalizeNucleosomeMutationCounts.R
+++ R_packages/nucperiodR/R/NormalizeNucleosomeMutationCounts.R
@@ -9,20 +9,34 @@ normalizeNucleosomeMutationCounts = function(rawCountsFilePath, backgroundCounts
   backgroundCounts = data.table::fread(file = backgroundCountsFilePath)
 
   # Make sure that the raw and background counts both use the same number of dyad positions.
-
   if (dim(rawCounts)[1] != dim(backgroundCounts)[1]) {
     stop("Unequal dyad positions in raw vs. background counts data.")
   }
+
+  # If the background file is "custom input" it will have the same column names as a raw counts file.
+  # Check for the raw counts headers, and replace them with the expected headers if necessary.
+  if (colnames(backgroundCounts)[2] == "Plus_Strand_Counts") {
+    colnames(backgroundCounts) = c("Dyad_Position", "Expected_Mutations_Plus_Strand",
+                                   "Expected_Mutations_Minus_Strand", "Expected_Mutations_Both_Strands")
+  }
+
+  # Compute a factor to adjust normalized values based on the ratio of total
+  # background:raw nucleosome mutation counts.
+  totalCountsAdjust = sum(backgroundCounts$Expected_Mutations_Both_Strands) /
+                      sum(rawCounts$Both_Strands_Counts)
 
   # Create a table of normalized values from the given data
   normalizedData = data.table::data.table()
   normalizedData[,Dyad_Position := backgroundCounts$Dyad_Position]
   normalizedData[,Normalized_Minus_Strand := mapply(normalize,rawCounts$Minus_Strand_Counts,
-                                                    backgroundCounts$Expected_Mutations_Minus_Strand)]
+                                                    backgroundCounts$Expected_Mutations_Minus_Strand,
+                                                    MoreArgs = list(totalCountsAdjust = totalCountsAdjust))]
   normalizedData[,Normalized_Plus_Strand := mapply(normalize,rawCounts$Plus_Strand_Counts,
-                                                   backgroundCounts$Expected_Mutations_Plus_Strand)]
+                                                   backgroundCounts$Expected_Mutations_Plus_Strand,
+                                                   MoreArgs = list(totalCountsAdjust = totalCountsAdjust))]
   normalizedData[,Normalized_Both_Strands := mapply(normalize,rawCounts$Both_Strands_Counts,
-                                                    backgroundCounts$Expected_Mutations_Both_Strands)]
+                                                    backgroundCounts$Expected_Mutations_Both_Strands,
+                                                    MoreArgs = list(totalCountsAdjust = totalCountsAdjust))]
 
   # Add a column for aligned, normalized strands
   normalizedData[,Normalized_Aligned_Strands :=
@@ -39,9 +53,8 @@ normalizeNucleosomeMutationCounts = function(rawCountsFilePath, backgroundCounts
 }
 
 # Normalizes data by dividing raw by expected, except in the case where expected is 0.
-# In this case, raw will also be 0, or at least some mutations would be expected, so
-# the returned value can safely be set to 0 instead of dividing by 0.
-normalize = function(raw, expected) {
+# (To avoid dividing by zero)
+normalize = function(raw, expected, totalCountsAdjust) {
   if (expected == 0) return(0)
-  else return(raw/expected)
+  else return(raw/expected*totalCountsAdjust)
 }
