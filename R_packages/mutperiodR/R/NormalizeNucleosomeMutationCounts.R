cac06a5bdbfa2a6c164d1b3a0a0d4cc7a91e330e
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-06-02 16:10:12 -0700

    Normalization is robust to mixing integer and half-base positions
    - If either the background or raw counts provided use half-base positions when the other does not, the half-base positions are spread out to the two adjacent bases (ignoring the positions on the ends)
    - Note: This really only works if both files are ENTIRELY half-base or integer, for a few different reasons.  I think it's unlikely that this kind of fix will be necessary otherwise though.

--- R_packages/mutperiodR/R/NormalizeNucleosomeMutationCounts.R
+++ R_packages/mutperiodR/R/NormalizeNucleosomeMutationCounts.R
@@ -8,17 +8,42 @@ normalizeNucleosomeMutationCounts = function(rawCountsFilePath, backgroundCounts
   rawCounts = data.table::fread(file = rawCountsFilePath)
   backgroundCounts = data.table::fread(file = backgroundCountsFilePath)
 
-  # Make sure that the raw and background counts both use the same number of dyad positions.
-  if (dim(rawCounts)[1] != dim(backgroundCounts)[1]) {
-    stop("Unequal dyad positions in raw vs. background counts data.")
-  }
-
   # If the background file is "custom input" it will have the same column names as a raw counts file.
   # Check for the raw counts headers, and replace them with the expected headers if necessary.
   if (colnames(backgroundCounts)[2] == "Plus_Strand_Counts") {
     colnames(backgroundCounts) = c("Dyad_Position", "Expected_Mutations_Plus_Strand",
                                    "Expected_Mutations_Minus_Strand", "Expected_Mutations_Both_Strands",
                                    "Expected_Mutations_Aligned_Strands")
+  }
+
+  # Check to see if one of the tables has half-base positions while the other does not.
+  # If this turns out to be the case, lower the resolution of the half-base positions by
+  # splitting them up into the two adjacent integer positions.  Also warn the user.
+  if (as.integer(backgroundCounts[[1]][1]) != as.numeric(backgroundCounts[[1]][1]) &&
+      as.integer(rawCounts[[1]][1]) == as.numeric(rawCounts[[1]][1])) {
+
+    warning("Background counts use half-base positions but raw counts do not.  ",
+            "Splitting background counts into adjacent integer positions and omitting a base on each end.")
+    rawCounts = rawCounts[c(-1,-dim(rawCounts)[1])]
+
+    backgroundCounts = data.table::rbindlist(lapply(1:(dim(backgroundCounts)[1] - 1),
+                                                    function(x) (backgroundCounts[x] + backgroundCounts[x+1]) / 2))
+
+  } else if (as.integer(backgroundCounts[[1]][1]) == as.numeric(backgroundCounts[[1]][1]) &&
+             as.integer(rawCounts[[1]][1]) != as.numeric(rawCounts[[1]][1])) {
+
+    warning("Raw counts use half-base positions but background counts do not.  ",
+            "Splitting raw counts into adjacent integer positions and omitting a base on each end.")
+    backgroundCounts = backgroundCounts[c(-1,-dim(backgroundCounts)[1])]
+
+    rawCounts = data.table::rbindlist(lapply(1:(dim(rawCounts)[1] - 1),
+                                             function(x) (rawCounts[x] + rawCounts[x+1]) / 2))
+
+  }
+
+  # Make sure that the raw and background counts both use the same number of dyad positions.
+  if (dim(rawCounts)[1] != dim(backgroundCounts)[1]) {
+    stop("Unequal dyad positions in raw vs. background counts data.")
   }
 
   # Compute a factor to adjust normalized values based on the ratio of total
