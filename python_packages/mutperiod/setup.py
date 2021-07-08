e6fc3fc7d406caf4dc13eb21a4e8ac73632490b0
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-07-08 11:26:29 -0700

    Prepare packaging files for v0.7.0

--- python_packages/mutperiod/setup.py
+++ python_packages/mutperiod/setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name="mutperiodpy",
-    version="0.5",
+    version="0.7.0",
     description='Analyze mutational periodicity about nucleosomes',
     long_description_content_type="text/markdown",
     url='https://github.com/bmorledge-hampton19/Nucleosome-Mutation-Analysis',
@@ -15,9 +15,10 @@ setup(
     python_requires='>=3.7',
     packages=find_packages(),
     package_data={"mutperiodpy": ["run_mutperiodR/*.r", "run_mutperiodR/*.R", "Tkinter_scripts/test_tube.png",
-                  "input_parsing/default_BPDE_lesion_calling.tsv"]},
+                  "input_parsing/*.tsv"]},
     entry_points=dict(
         console_scripts=['mutperiod=mutperiodpy.Main:main']
     ),
+    install_requires=["benbiohelpers"]
     
 )
