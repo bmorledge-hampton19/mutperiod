185eefcb44c2a61b24b1e1a35af8f849cf36472e
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2022-07-05 13:29:51 -0700

    Multiple fixes/tweaks to data directory generation
    - Checks to see if the data directory from the relevant text file actually points to a directory, not just an existing path
    - Ensured that if the data directory no longer exists, the user is immediately prompted to recreate/find it

--- python_packages/mutperiod/mutperiodpy/helper_scripts/UsefulFileSystemFunctions.py
+++ python_packages/mutperiod/mutperiodpy/helper_scripts/UsefulFileSystemFunctions.py
@@ -19,42 +19,44 @@ def getDataDirectory():
             
             # Double check to make sure the data directory is still intact.  
             # If it isn't, inform the user, and progress through the function to recreate it.
-            if not os.path.exists(dataDirectory):
+            if not os.path.isdir(dataDirectory):
                 print("Data directory not found at expected location: {}".format(dataDirectory))
                 print("Please select a new location to create a data directory.")
             else: return dataDirectory
 
-    else:
+    # Create a simple dialog to select a new data directory location.
+    # NOTE: The following code is not part of an else statement because the above "if" block will return
+    # the data directory if it proceeds correctly, and if it doesn't, the data directory text file
+    # needs to be recreated anyway.
+    from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog, Selections
+    checkDirs(os.path.dirname(dataDirectoryTextFilePath))
+    dialog = TkinterDialog(workingDirectory = os.path.dirname(dataDirectoryTextFilePath))
+    dialog.createFileSelector("Location to create new data directory:",0,("Fasta Files",".fa"), directory = True)
 
-        # Create a simple dialog to select a new data directory location.
-        from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog, Selections
-        checkDirs(os.path.dirname(dataDirectoryTextFilePath))
-        dialog = TkinterDialog(workingDirectory = os.path.dirname(dataDirectoryTextFilePath))
-        dialog.createFileSelector("Location to create new data directory:",0,("Fasta Files",".fa"), directory = True)
+    # Run the UI
+    dialog.mainloop()
 
-        # Run the UI
-        dialog.mainloop()
+    # If no input was received (i.e. the UI was terminated prematurely), then quit!
+    if dialog.selections is None: quit()
 
-        # If no input was received (i.e. the UI was terminated prematurely), then quit!
-        if dialog.selections is None: quit()
+    selections: Selections = dialog.selections
+    dataDirectoryDirectory = selections.getIndividualFilePaths()[0]
 
-        selections: Selections = dialog.selections
-        dataDirectoryDirectory = selections.getIndividualFilePaths()[0]
+    # Make sure a valid, writeable directory was given.  Then create the new directory (if it doesn't exist already), 
+    # write it to the text file, and return it!  (Also create the __external_data directory.)
+    if not os.path.exists(dataDirectoryDirectory): 
+        raise UserInputError("Given directory: " + dataDirectoryDirectory + " does not exist.")
 
-        # Make sure a valid, writeable directory was given.  Then create the new directory (if it doesn't exist already), 
-        # write it to the text file, and return it!  (Also create the __external_data directory.)
-        if not os.path.exists(dataDirectoryDirectory): 
-            raise UserInputError("Given directory: " + dataDirectoryDirectory + " does not exist.")
-
-        dataDirectory = os.path.join(dataDirectoryDirectory,"mutperiod_data")
-        try:
-            checkDirs(dataDirectory)
-            checkDirs(os.path.join(dataDirectory,"__external_data"))
-        except IOError:
-            raise InvalidPathError(dataDirectoryDirectory, "Given location for data directory is not writeable:")
-        with open(dataDirectoryTextFilePath, 'w') as dataDirectoryTextFile:
-            dataDirectoryTextFile.write(dataDirectory + '\n')
-        return dataDirectory
+    dataDirectory = os.path.join(dataDirectoryDirectory,"mutperiod_data")
+    try:
+        checkDirs(dataDirectory)
+        checkDirs(os.path.join(dataDirectory,"__external_data"))
+    except IOError:
+        raise InvalidPathError(dataDirectoryDirectory, "Given location for data directory is not writeable:")
+    with open(dataDirectoryTextFilePath, 'w') as dataDirectoryTextFile:
+        dataDirectoryTextFile.write(dataDirectory + '\n')
+    getExternalDataDirectory()
+    return dataDirectory
 
 
 # Get the external data directory, creating it if necessary.
