cfcec5664c74e2f4ed06c5934fa225b6aea9b00f
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2022-12-02 12:17:20 -0800

    Create GetNRL.py
    This script extracts an NRL from a given nucleosome map, without the need to create a full project.

---
+++ python_packages/mutperiod/mutperiodpy/quick_scripts/GetNRL.py
@@ -0,0 +1,35 @@
+# This script extracts an NRL from a given nucleosome map, without the need to create a full project.
+import os, subprocess
+from typing import List
+from benbiohelpers.FileSystemHandling.DirectoryHandling import getIsolatedParentDir
+from mutperiodpy.CountNucleosomePositionMutations import countNucleosomePositionMutations
+from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import rScriptsDirectory, getDataDirectory
+from benbiohelpers.TkWrappers.TkinterDialog import TkinterDialog
+
+
+def getNRL(nucleosomeMapFilePaths: List[str]):
+
+    NRLs = list()
+
+    for nucMapFilePath in nucleosomeMapFilePaths:
+        print(f"Generating nucleosome repeat length file for {os.path.basename(nucMapFilePath)}...")
+        nucMapRepeatLengthFilePath = nucMapFilePath.rsplit('.',1)[0] + "_repeat_length.txt"
+        nucMapSelfCountsFilePath = countNucleosomePositionMutations((nucMapFilePath,), (getIsolatedParentDir(nucMapFilePath),),
+                                                                    None, None, None)[0]
+        subprocess.run(("Rscript",os.path.join(rScriptsDirectory,"GetNucleosomeRepeatLength.R"),
+                        nucMapSelfCountsFilePath, nucMapRepeatLengthFilePath), check = True)
+        with open(nucMapRepeatLengthFilePath, 'r') as nucMapRepeatLengthFile:
+            NRLs.append(float(nucMapRepeatLengthFile.readline().strip()))
+
+    return NRLs
+
+
+def main():
+
+    with TkinterDialog(workingDirectory = getDataDirectory()) as dialog:
+        dialog.createMultipleFileSelector("Nucleosome Maps:", 0, "nucleosome_map.bed", ("Bed Files", ".bed"))
+
+    getNRL(dialog.selections.getFilePathGroups()[0])
+
+
+if __name__ == "__main__": main()
